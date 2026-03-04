1710bc75be7b8f747c7631925d73313c7de870bf
trale97 <thutrale1997@gmail.com>
2026-03-04 12:52:39 +0100

    function to retain number of factors using various methods

---
+++ R/factor_retention.R
@@ -0,0 +1,34 @@
+#' Function to retain number of factors using various methods.
+#'
+#' @param data A NxJ matrix of standardized items.
+#'
+#' @returns Number of factors.
+#' @export
+#'
+#' @examples factor_retention(X)
+factor_retention <- function(data){
+  tmp <- capture.output(
+    Q <- psych::fa.parallel(data, plot = F)
+  )
+  Q_parallel <- Q$nfact
+  Q_parallel_comp <- Q$ncomp
+
+  # using scree plot
+  pX <- PCAtools::pca(data, removeVar = .1)
+  Q_elbow <- PCAtools::findElbowPoint(pX$variance)
+
+  # factors retention using kaiser rule
+  ev <- eigen(cor(data))$values
+  Q_kaiser <- sum(ev > 1)
+
+  number_factors <- list()
+  number_factors$parallel <- Q_parallel
+  number_factors$parallel_comp <- Q_parallel_comp
+  number_factors$scree <- Q_elbow
+  number_factors$kaiser <- Q_kaiser
+  attr(number_factors, "class") <- "nfactors"
+
+  return(number_factors)
+}
+
+
