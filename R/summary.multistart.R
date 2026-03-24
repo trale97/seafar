9c3cb07af00a0b873f8cdc3d86c072425f6e32d5
trale97 <thutrale1997@gmail.com>
2026-03-24 02:18:10 +0100

    add option to the summary function to print a certain number of rows of the loading matrix for seafar_multistart()

--- R/summary.multistart.R
+++ R/summary.multistart.R
@@ -3,13 +3,14 @@
 #' @param object Object of class inheriting from 'multistart'.
 #' @param disp The default is \code{"loadings"} which returns only the estimated loadings matrix.
 #'             Otherwise if \code{"full"}, function returns both the estimated loadings and factor scores matrices.
+#' @param nrow Number of rows of the loading matrix to be displayed, default is 10.
 #' @param ...  Argument to be passed to or from other methods.
 #' @export
 #' @examples
 #'\dontrun{
 #' summary(big5_result, disp = "full")
 #'}
-summary.multistart <- function(object, disp = "loadings", ...){
+summary.multistart <- function(object, disp = "loadings", nrow = 10, ...){
   if (missing(disp)){
     disp = "loadings"
   }
@@ -18,7 +19,7 @@ summary.multistart <- function(object, disp = "loadings", ...){
     cat(sprintf("\nThe number of nonzero loadings is: %s\n",
                 sum(round(object$loadings,3) != 0)))
     cat(sprintf("\nThe estimated loadings matrix is \n"))
-    print(head(round(object$loadings,3),10))
+    print(head(round(object$loadings,3), nrow))
   } else if (disp == "full") {
     cat(sprintf("\nThe number of nonzero loadings is: %s\n",
                 sum(round(object$loadings,3) != 0)))
