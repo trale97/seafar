a7c296929914c3fa586a69fd85f00dcf7122c8a5
trale97 <thutrale1997@gmail.com>
2026-04-28 15:31:31 +0200

    added loop for model selection for LASSO penalty

--- R/is.seafar_lasso.R
+++ R/is.seafar_lasso.R
@@ -1,11 +1,12 @@
-#' Model selection for SEAFAR with LASSO penalty using the Index of Sparseness.
+#' Function to calculate the IS for each LASSO penalty parameter.
 #'
-#' @param data A data frame containing the dataset (standardized).
+#' @param data Data matrix of standardized items NxJ.
 #' @param nfactors The number of factors.
 #' @param lambda LASSO penalty tuning parameter.
 #' @param maxiter Maximum number of iterations of the AO procedure.
 #' @param eps Convergence criterion based on difference in loss between iterates.
 #' @param INIT Method to initialize loadings.
+#' @param nstarts Number of starts.
 #'
 #' @returns A list with
 #'\item{value}{IS value.}
@@ -17,7 +18,7 @@
 #'
 #' @examples
 #'\dontrun{
-#' IS <- is.seafar_lasso(
+#' IS <- IS_lasso(
 #'   data = X,
 #'   nfactors = 5,
 #'   lambda = 100,
@@ -25,13 +26,13 @@
 #'   nstarts = 50)
 #'}
 #'
-is.seafar_lasso <- function(data,
-                            nfactors,
-                            lambda,
-                            maxiter,
-                            eps,
-                            INIT,
-                            nstarts){
+IS_lasso <- function(data,
+                     nfactors,
+                     lambda,
+                     maxiter = 50,
+                     eps = 10^-4,
+                     INIT,
+                     nstarts){
   J <- dim(data)[2]
 
   VarSelect0 <- seafar_lasso_multistart(data = data,
@@ -71,5 +72,80 @@ is.seafar_lasso <- function(data,
   IS$maxsdP <- max(apply(P_hat, 2, sd))
   return(IS)
 }
+
+#' Model selection procedure with IS to choose the LASSO penalty paramter.
+#'
+#' @param data Data matrix of standardized items NxJ.
+#' @param nfactors The number of factors.
+#' @param max_lambda Maximum LASSO penalty parameter.
+#' @param maxiter Maximum number of iterations of the AO procedure.
+#' @param eps Convergence criterion based on difference in loss between iterates.
+#' @param INIT Method to initialize loading matrix.
+#' @param nstarts Number of starts.
+#'
+#' @returns The selected LASSO penalty parameter.
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#' lambda <- is.seafar_lasso(
+#'   data = X,
+#'   nfactors = 5,
+#'   max_lambda = 100,
+#'   INIT = 'mixed',
+#'   nstarts = 50)
+#'}
+is.seafar_lasso <- function(data,
+                     nfactors,
+                     max_lambda,
+                     maxiter = 50,
+                     eps = 10^-4,
+                     INIT,
+                     nstarts) {
+  N <- dim(data)[2]
+  I <- dim(data)[1]
+
+  lasso_vec <- seq(0.1*N, max_lambda, length.out = 50)
+  K <- length(lasso_vec)
+
+  avec <- matrix(nrow = K, ncol = 6)
+  colnames(avec) <- c( "lambda", "IS", "PEV", "Prop0", "MinNonZeroL", "MaxSDL")
+  for (k in 1:K) {
+    # retry loop
+    success <- FALSE
+    while (!success) {
+      res <- tryCatch(
+        IS_lasso(
+          data = data,
+          nfactors = nfactors,
+          lambda = max_lambda,
+          maxiter = maxiter,
+          eps = eps,
+          INIT = INIT,
+          nstarts = nstarts
+        ),
+        error = function(e) {
+          message(sprintf("k = %d: IS_lasso failed (lambda = %g), retrying...",
+                          k, lasso_vec[k]))
+          NULL
+        }
+      )
+      if (!is.null(res)) {
+        a       <- res
+        success <- TRUE
+      }
+      # else it loops and tries again
+    }
+    avec[k, ] <- c(
+      lasso_vec[k], a$value, a$vaf, a$propzero,
+      a$smallestP, a$maxsdP
+    )
+  }
+
+  sel_lasso <- lasso_vec[which.max(avec[,2])]
+
+  return(sel_lasso)
+}
+
 
 
